543c1c38270c7816694110c48486870e4cc32e88
Teresa Tetens <teresa.tetens@gmail.com>
2020-11-09 17:29:09 +0100

    Merge data from two APIs

--- server.js
+++ server.js
@@ -4,22 +4,34 @@ const bodyParser = require('body-parser')
 const jsonData = require('./static/file.json')
 const cors = require('cors');
 const morgan = require('morgan');
+const axios = require('axios');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('common'));
 
 app.get('/', (req, res)=>{
-    res.send('Henry Kene Teresa  Ben are running')
+    res.send('Henry Kene Teresa Ben are running')
 })
 app.get('/pokemon', (req, res, next)=>{
     res.send(jsonData)    
 })
 
-app.get('/pokemon/:id', (req, res, next)=>{
+app.get('/pokemon/:id', async (req, res, next)=>{
     const {id} = req.params;
-    const result = jsonData.filter(obj=> Number(id) === obj.id)
-    res.send(result);
+    const result = jsonData.find(obj=> Number(id) === obj.id)
+    try {
+        axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${result.name.english.toLowerCase()}/`)
+        .then(({data:pokedata}) => {
+           // console.log(pokedta)
+            result.pokedata = pokedata;
+            res.send(result);
+        })
+    } catch(error){
+        res.status(404).send('Oppps! There is no matching Pokemon!')
+    } 
+
 })
 
 app.get('/pokemon/:id/:info', (req, res, next)=>{
