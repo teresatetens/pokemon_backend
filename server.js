b73757838a3f2f86416297ae05cf7a3097adc1aa
Teresa Tetens <teresa.tetens@gmail.com>
2020-11-11 17:23:10 +0100

    fetch from API and join two objects

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
-const jsonData = require('./static/file.json')
+const pokedex = require('./static/file.json')
 const cors = require('cors');
 const morgan = require('morgan');
 const axios = require('axios');
@@ -10,16 +10,30 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('common'));
 
-app.get('/', (req, res)=>{
-    res.send('Henry Kene Teresa Ben are running')
-})
+// app.get('/', (req, res)=>{
+//     res.send('Henry Kene Teresa Ben are running')
+// })
 app.get('/pokemon', (req, res, next)=>{
-    res.send(jsonData)    
+
+    const promiseOfBen = pokedex.slice(0, 25).map( async (poke)=>{
+        return await axios.get(`https://pokeapi.co/api/v2/pokemon/${poke.name.english.toLowerCase()}/`)
+    })
+    Promise.all(promiseOfBen).then((resolvedData)=>{
+        const FullPokeData = [];
+        resolvedData.map((resolvedPoke, index)=>{
+            FullPokeData.push({
+                pokeDexData: pokedex[index],
+                pokeApiData: resolvedPoke.data
+            })
+        })
+        console.log(FullPokeData);
+        res.send(FullPokeData);
+    })
 })
 
 app.get('/pokemon/:id', async (req, res, next)=>{
     const {id} = req.params;
-    const result = jsonData.find(obj=> Number(id) === obj.id)
+    const result = pokedex.find(obj=> Number(id) === obj.id)
     try {
         axios
         .get(`https://pokeapi.co/api/v2/pokemon/${result.name.english.toLowerCase()}/`)
@@ -31,24 +45,23 @@ app.get('/pokemon/:id', async (req, res, next)=>{
     } catch(error){
         res.status(404).send('Oppps! There is no matching Pokemon!')
     } 
-
 })
 
-app.get('/pokemon/:id/:info', (req, res, next)=>{
-    console.log(req.params);
-    const {id, info} = req.params;
+// app.get('/pokemon/:id/:info', (req, res, next)=>{
+//     console.log(req.params);
+//     const {id, info} = req.params;
 
-    const result = jsonData.filter(obj=> Number(id) === obj.id)
+//     const result = jsonData.find(obj=> Number(id) === obj.id)
     
-    if(info==="name"){
-        res.send(result[0].name)
-    } else if(info==="type"){
-    res.send(result[0].type)
-    } else if(info==="base"){
-    res.send(result[0].base)
-    }
+//     if(info==="name"){
+//         res.send(result[0].name)
+//     } else if(info==="type"){
+//     res.send(result[0].type)
+//     } else if(info==="base"){
+//     res.send(result[0].base)
+//     }
     
-})
+// })
 const port = process.env.PORT || 3003;
 app.listen(port, ()=>{
     console.log(`Server linstening on port: ${port}`);
